86e07f02bed8272ef02c3309d5cffcf100ebc514
zhonghui <zhong@haowu.com>
2021-03-20 15:32:18 +0800

    aa

--- pages/index.vue
+++ pages/index.vue
@@ -12,7 +12,7 @@
           rel="noopener noreferrer"
           class="button--green"
         >
-          Documentation
+          Documentationaaa
         </a>
         <a
           href="https://github.com/nuxt/nuxt.js"
